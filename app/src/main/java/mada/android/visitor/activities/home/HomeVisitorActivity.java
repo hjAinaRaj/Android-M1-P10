b1e4670962ecf07cf5eff02500fa1835c3fcbc29
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-07-25 13:56:25 +0300

    Homepage activity with menu widget

---
+++ app/src/main/java/mada/android/visitor/activities/home/HomeVisitorActivity.java
@@ -0,0 +1,16 @@
+package mada.android.visitor.activities.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mada.android.R;
+
+public class HomeVisitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_visitor);
+    }
+}
