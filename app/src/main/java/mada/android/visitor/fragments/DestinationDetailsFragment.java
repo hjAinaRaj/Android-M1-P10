4c1f61af02e0ec815212740a65166dc39975a7ba
hjAinaRaj <112469049+hjAinaRaj@users.noreply.github.com>
2023-07-25 22:30:44 +0300

    add html content

--- app/src/main/java/mada/android/visitor/fragments/DestinationDetailsFragment.java
+++ app/src/main/java/mada/android/visitor/fragments/DestinationDetailsFragment.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import mada.android.R;
 import mada.android.models.Destination;
@@ -17,7 +19,8 @@ import mada.android.models.Destination;
  * create an instance of this fragment.
  */
 public class DestinationDetailsFragment extends Fragment {
-
+    private WebView webView;
+    private Destination destination;
 
     public DestinationDetailsFragment() {
         // Required empty public constructor
@@ -27,7 +30,7 @@ public class DestinationDetailsFragment extends Fragment {
     public static DestinationDetailsFragment newInstance(Destination destination) {
         DestinationDetailsFragment fragment = new DestinationDetailsFragment();
         Bundle args = new Bundle();
-
+        fragment.destination = destination;
         fragment.setArguments(args);
         return fragment;
     }
@@ -36,12 +39,48 @@ public class DestinationDetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_destination_details, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_destination_details, container, false);
+        webView =  view.findViewById(R.id.destinationDetailsWebView);
+
+        // Enable JavaScript
+        //webView.getSettings().setJavaScriptEnabled(true);
+
+        // Load and display the HTML content
+        String htmlContent = "<html>" +
+                "<body>\n" +
+                "    <h2>Rova of Manjakamiadana</h2>\n" +
+                "    <p>\n" +
+                "        The Rova of Manjakamiadana, also known as the Queen's Palace or the Royal Palace of Antananarivo,\n" +
+                "        is a historic palace located in Antananarivo, Madagascar. It served as the residence of the\n" +
+                "        sovereigns of the Kingdom of Madagascar during the 17th and 18th centuries.\n" +
+                "    </p>\n" +
+                "    <p>\n" +
+                "        The palace complex is perched on the highest hill in the city, providing a commanding view\n" +
+                "        of Antananarivo. It features traditional Merina architecture and is characterized by its\n" +
+                "        distinctive wooden balconies and steep roofs.\n" +
+                "    </p>\n" +
+                "    <p>\n" +
+                "        The Rova of Manjakamiadana was severely damaged by a fire in 1995, but restoration efforts\n" +
+                "        have been ongoing to preserve this important historical site.\n" +
+                "    </p>\n" +
+                "    <p>\n" +
+                "        Visitors can explore the Rova to learn about the rich history of Madagascar and its royal heritage.\n" +
+                "        It remains a symbol of national identity and a cultural treasure for the people of Madagascar.\n" +
+                "    </p>\n" +
+                "</body>" +
+                "</html>";
+        webView.loadData(htmlContent, "text/html", "UTF-8");
+
+        //Handle URL clicks within the WebView
+        webView.setWebViewClient(new WebViewClient());
+        return view;
     }
 }
