ebd5eb61a4f62c3bf180a8e75760656592335c04
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-07-26 20:00:07 +0300

    savetoken

---
+++ app/src/main/java/mada/android/administrator/activities/home/HomeAdminActivity.java
@@ -0,0 +1,16 @@
+package mada.android.administrator.activities.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mada.android.R;
+
+public class HomeAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_admin2);
+    }
+}
