742fb7235f847b8facb5a1abb669a883a3f2ddf2
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-07-27 23:40:50 +0300

    Update MyFirebaseMessagingService.java

--- app/src/main/java/mada/android/services/extern/MyFirebaseMessagingService.java
+++ app/src/main/java/mada/android/services/extern/MyFirebaseMessagingService.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -15,22 +16,32 @@ import mada.android.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String CANAL = "MyNotifCanal";
+    private static final String TOPIC = "all_users_topic";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage message) {
         super.onMessageReceived(message);
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        showNotification(message);
+                    }
+                });
+    }
 
+    private void showNotification(RemoteMessage message) {
+        String notificationTitle = message.getNotification().getTitle();
         String notificationMessage = message.getNotification().getBody();
 
         // Create notification
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CANAL);
-        notificationBuilder.setContentTitle("Notification app MADA");
+        notificationBuilder.setContentTitle(notificationTitle);
         notificationBuilder.setContentText(notificationMessage);
 
         //user icon
         notificationBuilder.setSmallIcon(R.drawable.splash_screen);
 
-        // Send notif
+        // Send notification
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
