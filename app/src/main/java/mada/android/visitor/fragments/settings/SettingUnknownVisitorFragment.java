fe969b889c44dfffc7ec06c38a2aafc2a3bf9257
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-07-30 11:57:51 +0300

    set language

--- app/src/main/java/mada/android/visitor/fragments/settings/SettingUnknownVisitorFragment.java
+++ app/src/main/java/mada/android/visitor/fragments/settings/SettingUnknownVisitorFragment.java
@@ -1,5 +1,7 @@
 package mada.android.visitor.fragments.settings;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatDelegate;
@@ -9,10 +11,16 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.Locale;
 
 import mada.android.R;
 import mada.android.commons.activities.AuthActivity;
@@ -23,6 +31,8 @@ public class SettingUnknownVisitorFragment extends BaseFragment {
     private Button buttonToLogin;
     private Switch switchNotification;
     private Switch switchNightMode;
+    private Spinner spinnerLanguage;
+    private Button buttonToEn;
     private MyFirebaseMessagingService myFirebaseMessagingService;
 
     public SettingUnknownVisitorFragment() {
@@ -51,6 +61,15 @@ public class SettingUnknownVisitorFragment extends BaseFragment {
         this.buttonToLogin = (Button) view.findViewById(R.id.buttonToLogin);
         this.switchNotification = (Switch) view.findViewById(R.id.switchNotif);
         this.switchNightMode = (Switch) view.findViewById(R.id.switchNightMode);
+        this.spinnerLanguage = (Spinner) view.findViewById(R.id.spinnerLanguage);
+        this.buttonToEn = (Button) view.findViewById(R.id.buttonToEn);
+
+        this.buttonToEn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchLanguage("en");
+            }
+        });
         this.buttonToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -79,6 +98,45 @@ public class SettingUnknownVisitorFragment extends BaseFragment {
                 updateTheme(view, isChecked);
             }
         });
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this.getContext(),
+                R.array.language_arrays,
+                android.R.layout.simple_spinner_item
+        );
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerLanguage.setAdapter(adapter);
+        spinnerLanguage.setSelection(0);
+
+        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                // Récupérez la langue sélectionnée
+                String selectedLanguage = parent.getItemAtPosition(position).toString();
+                selectedLanguage = "en";
+
+                // Changez la langue de l'application en fonction de la sélection
+                //switchLanguage(selectedLanguage);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void switchLanguage(String languageCode) {
+        // Changez la configuration de la langue en fonction du code de langue sélectionné
+        Locale locale = new Locale(languageCode);
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+        this.getActivity().recreate();
     }
 
     public void updateTheme(View view, boolean isChecked){
