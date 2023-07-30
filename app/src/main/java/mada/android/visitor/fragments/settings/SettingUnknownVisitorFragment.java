518405c0677f03998462209de8b368355847b5c2
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-07-30 09:14:56 +0300

    activate notif swithInput + frameLayout -> Relative Layout

--- app/src/main/java/mada/android/visitor/fragments/settings/SettingUnknownVisitorFragment.java
+++ app/src/main/java/mada/android/visitor/fragments/settings/SettingUnknownVisitorFragment.java
@@ -8,16 +8,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import mada.android.R;
 import mada.android.commons.activities.AuthActivity;
 import mada.android.commons.fragments.BaseFragment;
+import mada.android.services.external.MyFirebaseMessagingService;
 
 public class SettingUnknownVisitorFragment extends BaseFragment {
     private Button buttonToLogin;
+    private Switch switchNotification;
+    private MyFirebaseMessagingService myFirebaseMessagingService;
 
     public SettingUnknownVisitorFragment() {
         // Required empty public constructor
+        if(myFirebaseMessagingService == null){
+            myFirebaseMessagingService = new MyFirebaseMessagingService();
+        }
     }
 
     @Override
@@ -37,10 +46,26 @@ public class SettingUnknownVisitorFragment extends BaseFragment {
 
     public void initWidget(View view){
         this.buttonToLogin = (Button) view.findViewById(R.id.buttonToLogin);
+        this.switchNotification = (Switch) view.findViewById(R.id.switchNotif);
         this.buttonToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startNewActivity(v, new AuthActivity());
+            }
+        });
+
+        this.switchNotification.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    if(isChecked){
+                        myFirebaseMessagingService.saveSavedToken();
+                    }else{
+                        myFirebaseMessagingService.unsubscribeSavedToken();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
