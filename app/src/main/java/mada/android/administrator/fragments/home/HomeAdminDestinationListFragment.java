4e52597f2e590d09b5163119029b2570d1fb3fd4
MC-Michel <mnakanyandriamihaja@gmail.com>
2023-08-04 09:21:11 +0300

    fix admin interface

--- app/src/main/java/mada/android/administrator/fragments/home/HomeAdminDestinationListFragment.java
+++ app/src/main/java/mada/android/administrator/fragments/home/HomeAdminDestinationListFragment.java
@@ -9,12 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import mada.android.R;
 import mada.android.administrator.fragments.destination.DestinationCreateFragment;
 import mada.android.commons.fragments.BaseFragment;
 
 public class HomeAdminDestinationListFragment extends BaseFragment {
     private Button button;
+    private FloatingActionButton floatingActionButton;
 
     public HomeAdminDestinationListFragment() {
         // Required empty public constructor
@@ -35,9 +38,17 @@ public class HomeAdminDestinationListFragment extends BaseFragment {
     }
 
     private void initWidget(View view){
-        this.button = (Button) view.findViewById(R.id.buttonCreateDestination);
+        /*this.button = (Button) view.findViewById(R.id.buttonCreateDestination);
 
         this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(R.id.fragmentContainerViewAdmin, new DestinationCreateFragment());
+            }
+        });*/
+
+        this.floatingActionButton = (FloatingActionButton) view.findViewById(R.id.fabButtonAdmin);
+        this.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 replaceFragment(R.id.fragmentContainerViewAdmin, new DestinationCreateFragment());
